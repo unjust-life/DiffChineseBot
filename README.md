de144e08ad0719dbbb0e6a4c18ce9ad271d6b79f
twlr <16621105214@qq.com>
2019-04-03 11:54:09 +0800

    v1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# DiffChineseBot
-字符串文本diff
+diffChineseBot
+===========
+
+## 介绍
+该模块对两个字符串进行比较并输出差异。
+
+它基于John Resig的Javascript Diff算法(http://ejohn.org/projects/javascript-diff-algorithm/)，并大量借鉴了该算法。谢谢你,约翰!
+
+主要区别在于，该模块支持在包含中文或其他使用多字节字符和/或不使用空格分隔单词的语言的字符串中查找差异。
+
+
+As an example here is how we at [KAWO.com](http://kawo.com) use this package in our app:
+
+![Image text](./diff-example.png)
+
+##使用方式
+
+### npm安装
+
+```bash
+$ npm install diffchinesebot
+```
+
+### RequireJS (AMD规范)
+```javascript
+require.config({
+	paths: {
+		diffChinese: './diffChineseBot/index.js'
+	}
+});
+
+require( [ 'diffChinese' ], function( diffChinese ) {
+	...
+});
+
+```
+
+### 直接引用
+```html
+<script src="./diffChineseBot.js"></script>
+<script>
+window.diffChinese(oldStr, newStr, point)
+</script>
+```
+
+## Usage
+
+diffChineseBot包只包含一个函数diffChinese，可以这样使用:
+```javascript
+var beforeString = 'the quick brown fox';
+var afterString = 'the quick brown dog';
+var point = 1
+
+var diff = diffChinese( beforeString, afterString, point );
+
+// => { before: 'the quick brown <del>fox</del>', after: 'the quick brown <ins>dog</ins>' }
+```
+
+第一个参数：旧字符串	String
+
+第二个参数：新字符串	String
+
+第三个参数：不匹配度 	Number 默认可不传 不匹配度高于设定将不再进行比对高亮
---
+++ index.js
@@ -0,0 +1,142 @@
+/*
+ * Javascript Diff Algorithm for Strings that might contain Chinese or other multi byte chacacters not separated by spaces.
+ * Author: Brian van Damme
+ * Date: March 10, 2015
+ *
+ * 这个库的源码是基于原作者Brian van Damme的diffChinese
+ * 根据业务需求进行改动的版本
+ */
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.diffChinese = factory();
+  }
+}(this, function () {
+  // ESCAPE CHARS
+  function escape(s) {
+    var n = s;
+    n = n.replace(/&/g, "&amp;");
+    n = n.replace(/</g, "&lt;");
+    n = n.replace(/>/g, "&gt;");
+    n = n.replace(/"/g, "&quot;");
+    return n;
+  }
+
+  // GET ALL THE SEPARATORS
+  function getSpaces(s) {
+    var ret = s.split(/[\u0000-\uffff]/);
+    if (ret == null) {
+      ret = ["\n"];
+    } else {
+      ret.push("\n");
+    }
+    return ret;
+  }
+
+  // FIND THE DIFFERENCES BETWEEN 2 ARRAYS OF WORDS. CREDIT GOES TO JOHN RESIG
+  function diff(o, n) {
+    var ns = new Object();
+    var os = new Object();
+
+    for (var i = 0; i < n.length; i++) {
+      if (ns[n[i]] == null)
+        ns[n[i]] = { rows: new Array(), o: null };
+      ns[n[i]].rows.push(i);
+    }
+
+    for (var i = 0; i < o.length; i++) {
+      if (os[o[i]] == null)
+        os[o[i]] = { rows: new Array(), n: null };
+      os[o[i]].rows.push(i);
+    }
+
+    for (var i in ns) {
+      if (ns[i].rows.length == 1 && typeof (os[i]) != "undefined" && os[i].rows.length == 1) {
+        n[ns[i].rows[0]] = { text: n[ns[i].rows[0]], row: os[i].rows[0] };
+        o[os[i].rows[0]] = { text: o[os[i].rows[0]], row: ns[i].rows[0] };
+      }
+    }
+
+    for (var i = 0; i < n.length - 1; i++) {
+      if (n[i].text != null && n[i + 1].text == null && n[i].row + 1 < o.length && o[n[i].row + 1].text == null &&
+        n[i + 1] == o[n[i].row + 1]) {
+        n[i + 1] = { text: n[i + 1], row: n[i].row + 1 };
+        o[n[i].row + 1] = { text: o[n[i].row + 1], row: i + 1 };
+      }
+    }
+
+    for (var i = n.length - 1; i > 0; i--) {
+      if (n[i].text != null && n[i - 1].text == null && n[i].row > 0 && o[n[i].row - 1].text == null &&
+        n[i - 1] == o[n[i].row - 1]) {
+        n[i - 1] = { text: n[i - 1], row: n[i].row - 1 };
+        o[n[i].row - 1] = { text: o[n[i].row - 1], row: i - 1 };
+      }
+    }
+
+    return { o: o, n: n };
+  }
+
+  // This example returns an object, but the module
+  // can return a function as the exported value.
+  return function (o, n, point) {
+
+    // TRIM 去除两端空格
+    o = o.replace(/\s+$/, '');
+    n = n.replace(/\s+$/, '');
+
+    // GET ALL THE WORDS IN THE STRING. SPLIT BY SPACE. SPLIT BY CHARACTER FOR CHINESE
+    // 字符串转数组传参
+    var out = diff(o == "" ? [] : o.match(/[\u0000-\uffff]/g), n == "" ? [] : n.match(/[\u0000-\uffff]/g));
+
+    // GET ALL THE IN BETWEEN STUFF
+    var oSpace = getSpaces(o);
+    var nSpace = getSpaces(n);
+
+    // 分析重复占比  新文本和旧文本的差异率 count
+    var count = 0;
+    for (var i = 0; i < out.n.length; i++) {
+      if (out.n[i].text == null) {
+        count++
+      }
+    }
+    count = count / out.n.length
+
+    // 如果差异率大于设定的point百分比值，将不再对文本进行高亮
+    if (point != undefined && count > point) {
+      return { before: o, after: n };
+    } else {
+      // REBUILD ORIGINAL STRING
+      var os = "";
+      for (var i = 0; i < out.o.length; i++) {
+        if (out.o[i].text != null) {
+          os += oSpace[i];
+          os += escape(out.o[i].text);
+        } else {
+          // WRAP IN <DEL> TAG IF REMOVED
+          os += "<span class='del-text'>" + oSpace[i] + escape(out.o[i]) + "</span>";
+        }
+      }
+
+      // REBUILD NEW STRING
+      var ns = "";
+      for (var i = 0; i < out.n.length; i++) {
+        if (out.n[i].text != null) {
+          ns += nSpace[i];
+          ns += escape(out.n[i].text);
+        } else {
+          // WRAP IN <INS> TAG IF NEW
+          ns += "<span class='new-text'>" + nSpace[i] + escape(out.n[i]) + "</span>";
+        }
+      }
+    }
+    return { before: os, after: ns };
+  };
+}));
